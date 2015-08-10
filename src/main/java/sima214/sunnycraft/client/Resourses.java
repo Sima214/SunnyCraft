8abc2db4669cdff4b0c1788dfa6768196b9f69ab
Sima214 <anastasios2014@gmail.com>
2015-08-10 17:03:50 +0300

    Scrap that

--- src/main/java/sima214/sunnycraft/client/Resourses.java
+++
@@ -1,9 +0,0 @@
-package sima214.sunnycraft.client;
-
-import sima214.sunnycraft.core.Constants;
-import net.minecraft.util.ResourceLocation;
-
-public class Resourses {
-	public static String SUN_MODEL_NAME=new ResourceLocation(Constants.MODID, "/models/sun.smodel").toString();
-	public static String SUN_SHADER_NAME=new ResourceLocation(Constants.MODID, "/shaders/sun").toString();
-}
--- src/main/java/sima214/sunnycraft/core/Constants.java
+++
@@ -1,6 +0,0 @@
-package sima214.sunnycraft.core;
-
-public class Constants {
-	public static final String VERSION="Ultralpha";
-	public static final String MODID="sunnycraft";
-}
